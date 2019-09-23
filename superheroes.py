b5522ab394c0243d285dab93066af51ca37cae6b
Patrick <w.patrick.kelly@gmail.com>
2019-09-23 13:29:13 -0700

    attack, armor and defend kinda work, I think there is a problum though

--- superheroes.py
+++ superheroes.py
@@ -12,7 +12,7 @@ class Ability:
 
 #attack method
     def attack(self):
-        attack_strength = random.randint(0,20)
+        attack_strength = random.randint(0,self.max_damage)
         print(attack_strength)
 
 #armon method
@@ -22,21 +22,37 @@ class Armor:
         self.max_block = max_block
 
     def block(self):
-        max_block = random.randint(0,20)
+        max_block = random.randint(0,self.max_block)
         print(max_block)
 
 class Hero:
-    def __init__(self, name,current_health, abilities, starting_health = 100,):
+    def __init__(self, name, starting_health = 100,):
         self.abilities = []
         self.armors = []
         self.name = name
-        self.current_health = current_health
+        self.current_health = starting_health
         self.starting_health = starting_health
 
     def add_ability(self, ability):
-         self.ability = Ability
+         #self.ability = Ability
          self.abilities.append(ability)
-         self.abilities.append(ability)
+
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage = ability.attack()
+            return total_damage
+
+    def add_armor(self,armor):
+        self.armors.append()
+
+    def defend(self, damage_amt):
+        total_blocked = 0
+        for armor in self.armors:
+            total_blocked += armor.block()
+
+        return total_blocked
 
 
 
@@ -44,13 +60,15 @@ class Hero:
 #prints
 if __name__ == "__main__":
     ability = Ability("Great Debugging", 50)
-    hero = Hero("Grace Hopper", 200,[])
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
     hero.add_ability(ability)
-    print(hero.abilities)
+    hero.add_ability(another_ability)
+    print(hero.attack())
+
 
     #print(my_hero.name)
     #print(my_hero.current_health)
-
 
 
     #ability = Ability("Debugging Ability", 20)
